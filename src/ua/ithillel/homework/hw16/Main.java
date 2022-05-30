8ca937a5447d0c6cab28c20b3048b240192b1d7a
artemm3708 <artemm37.08@gmail.com>
2022-05-30 18:21:30 +0300

    add annotation hw

--- src/ua/ithillel/homework/hw16/Main.java
+++ src/ua/ithillel/homework/hw16/Main.java
@@ -3,8 +3,6 @@ package ua.ithillel.homework.hw16;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Test1 test1 = new Test1();
-        Class<Test1> test1Class = Test1.class;
-        TestRunner.start(test1Class, test1);
+        TestRunner.start(TestClass.class);
     }
 }
